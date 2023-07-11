2516476e3a43c31af223eb13f39349f08fa51cca
Ane-Alx <abokhaiane.leadlearning@gmail.com>
2023-07-11 11:53:42 -0700

    one to four

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for askey in range(97, 123):
+    if chr(askey) != 'q' and chr(askey) != 'e':
+        print("{}".format(chr(askey)), end="")
---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for hex_n in range(0, 99):
+    print("{} = {}".format(hex_n, hex(hex_n)))
+    
